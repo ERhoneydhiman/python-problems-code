9060e23aaa9d83aaa733df6ab5b6f5d9e14bb4cf
ERhoneydhiman <mhoneyhiman@gmail.com>
2024-04-09 23:06:11 +0530

    add another method

--- TCS NQT/q13.py
+++ TCS NQT/q13.py
@@ -1,10 +1,21 @@
 # Que : check number is devided by 3 or not
 
 N = input()
-N = int(N)
+# N = int(N)
 
-if N % 3 == 0:
+# if N % 3 == 0:
+#     print('yes')
+# else:
+#     print('no')
+
+# another method
+
+sum = 0
+for i in N:
+    i = int(i)
+    sum += i
+
+if sum % 3 == 0:
     print('yes')
 else:
     print('no')
-    
