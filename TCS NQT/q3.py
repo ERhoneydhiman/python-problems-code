c4a1c12b65fc61e4581aabbcc2719a1534773f9d
ERhoneydhiman <mhoneyhiman@gmail.com>
2024-04-11 16:19:37 +0530

    chocolate que

--- TCS NQT/q3.py
+++ TCS NQT/q3.py
@@ -1,4 +1,5 @@
-# choklate
+# chocolate question
+
 array = []
 
 N = int(input("enter number of element: "))
@@ -8,16 +9,18 @@ print("Enter elements of the array :")
 for i in range (N):
     user_input = input("Enter an element: ")
     array.append(int(user_input))
+    
+    
+array_filled = []
+array_empty = []
 
-dct = {}
-
-for element in array:
-    if element in dct:
-        dct[element] +=1
-        
+for i in range(len(array)):
+    if array[i] != 0:
+        array_filled.append(array[i])
     else:
-        dct[element] = 1
+        array_empty.append(array[i])
         
-for j in dct:
-    if dct[j] % 2 == 0:
-        print(j, end=" ")
+array_filled.extend(array_empty)
+
+for i in range(len(array_filled)):
+    print(array_filled[i], end=" ")
